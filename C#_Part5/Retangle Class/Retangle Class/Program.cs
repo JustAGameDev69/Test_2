bf6f70051d6c36d3ab555f575d16d5f633d33c15
JustAGameDev69 <134581425+JustAGameDev69@users.noreply.github.com>
2023-07-19 21:12:21 +0700

    main

---
+++ C#_Part5/Retangle Class/Retangle Class/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Work_Hard
+{
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double getArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) / 2;
+        }
+
+        public string Display()
+        {
+            return "Rectangle detail :  {" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter rectangle width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Enter rectangle height: ");
+            double height = double.Parse(Console.ReadLine());
+            Rectangle rectangle1 = new Rectangle(width, height);
+
+            Console.WriteLine(rectangle1.Display());
+            Console.WriteLine("Area of this rectangle is: {0}", rectangle1.getArea());
+            Console.WriteLine("Perimeter of this rectangle is: {0}", rectangle1.GetPerimeter());
+        }
+    }
+
+
+}
